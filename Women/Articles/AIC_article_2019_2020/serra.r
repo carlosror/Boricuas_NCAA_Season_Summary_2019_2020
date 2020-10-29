a9cd89fb4e170cefaca840ae4c67150cb55611f7
Carlos <carlosgg123@gmail.com>
2020-10-29 10:54:56 -0400

    Update 2020-10-29

---
+++ Women/Articles/AIC_article_2019_2020/serra.r
@@ -0,0 +1,63 @@
+library(ggplot2)
+library(scales)
+
+Serra_stats_2018 <- read.csv("Andrea_Serra_stats_2018.csv")
+Serra_stats_2019 <- read.csv("Andrea_Serra_stats_2019.csv")
+
+year <- as.factor(c(2018, 2019))
+max_kills <- c(max(Serra_stats_2018$K), max(Serra_stats_2019$K))
+max_digs <- c(max(Serra_stats_2018$D), max(Serra_stats_2019$D))
+max_aces <- c(max(Serra_stats_2018$SA), max(Serra_stats_2019$SA))
+
+serra_df <- data.frame(year, max_kills, max_digs, max_aces)
+
+theme_plot <- function(...) {
+  # theme_minimal() +
+  theme( 
+    axis.title.x = element_blank(),
+    # axis.title.y = element_blank(),
+    axis.title.y = element_text(vjust=8, size = 14),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 12), 
+    panel.grid.major.x = element_blank(),
+    # panel.grid.major.y = element_blank(),
+    panel.grid.major.y = element_line(colour = "gray75"),
+    panel.grid.minor = element_blank(), 
+    # text = element_text(size=16), 
+    plot.title = element_text(hjust = 0.5, vjust = 10, size = 15), 
+    # plot.subtitle = element_text(hjust = -0.12, vjust = 10, , color = "orange"),
+    plot.margin = unit(c(2,1,1,1), "cm"),
+    panel.background = element_rect(fill = 'white', color = "white"),
+    # plot.background = element_rect(fill = 'gray95'),
+    panel.border = element_blank(),
+    legend.position = "none"
+  )
+}
+
+colorPalette <- c("gray", "gold")
+p_kills <- ggplot(serra_df) + geom_bar(aes(x=year, y = max_kills, fill = year), stat = "identity", width=0.4) +
+     scale_y_continuous(breaks = c(0, 21, 31), limits = c(0, 33)) +
+     # geom_text(aes(label = max_kills, x=year, y = max_kills), position = position_dodge(width = 0.75), vjust = -0.6, size = 4) + 
+     scale_fill_manual(values=colorPalette) +
+     theme_plot() + 
+     ggtitle(label = "Kills") + 
+     ylab("Single-match high")
+print(p_kills)
+
+p_digs <- ggplot(serra_df) + geom_bar(aes(x=year, y = max_digs, fill = year), stat = "identity", width=0.4) +
+     scale_y_continuous(breaks = c(0, 16, 28), limits = c(0, 30)) +
+     scale_fill_manual(values=colorPalette) +
+     theme_plot() + 
+     ggtitle(label = "Digs") + 
+     ylab("Single-match high")
+print(p_digs)
+
+p_aces <- ggplot(serra_df) + geom_bar(aes(x=year, y = max_aces, fill = year), stat = "identity", width=0.4) +
+     scale_y_continuous(breaks = c(0, 5, 9), limits = c(0, 11)) +
+     scale_fill_manual(values=colorPalette) +
+     theme_plot() + 
+     ggtitle(label = "Aces") + 
+     ylab("Single-match high")
+print(p_aces)
+
---
+++ Women/Articles/Andrea_Fuentes_article/fuentes.r
@@ -0,0 +1,51 @@
+library(ggplot2)
+library(scales)
+
+Fuentes_stats_df <- read.csv("Andrea_Fuentes_stats_2018_2019.csv")
+
+Fuentes_stats_df$year <- as.factor(Fuentes_stats_df$year)
+
+theme_plot <- function(...) {
+  # theme_minimal() +
+  theme( 
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    # axis.title.y = element_text(vjust=8, size = 14),
+    axis.ticks = element_blank(),
+    axis.text.x = element_text(size = 25),
+    axis.text.y = element_text(size = 25), 
+    panel.grid.major.x = element_blank(),
+    # panel.grid.major.y = element_blank(),
+    panel.grid.major.y = element_line(colour = "gray75"),
+    panel.grid.minor = element_blank(), 
+    # text = element_text(size=16), 
+    plot.title = element_text(hjust = 0.5, vjust = 10, size = 35), 
+    # plot.subtitle = element_text(hjust = -0.12, vjust = 10, , color = "orange"),
+    plot.margin = unit(c(2,1,1,1), "cm"),
+    panel.background = element_rect(fill = '#fed500', color = "#fed500"),
+    plot.background = element_rect(fill = "#fed500", color = "#fed500"),
+    # plot.background = element_rect(fill = 'gray95'),
+    panel.border = element_blank(),
+    legend.position = "none",
+  )
+}
+
+colorPalette <- c("gray", "black")
+p_assists_per_set <- ggplot(Fuentes_stats_df) + geom_bar(aes(x=year, y = assists_per_set, fill = year), stat = "identity", width=0.4) +
+     scale_y_continuous(breaks = c(0, 8.69, 11.77), limits = c(0, 12)) +
+     # geom_text(aes(label = max_kills, x=year, y = max_kills), position = position_dodge(width = 0.75), vjust = -0.6, size = 4) + 
+     scale_fill_manual(values=colorPalette) +
+     theme_plot() + 
+     ggtitle(label = "Assists per set") 
+     # ylab("Single-match high")
+print(p_assists_per_set)
+
+p_missouri_hit_pcntg <- ggplot(Fuentes_stats_df) + geom_bar(aes(x=year, y = team_hit_pcntg, fill = year), stat = "identity", width=0.4) +
+     scale_y_continuous(breaks = c(0.20, 0.263, 0.301), limits = c(0.205, 0.305), oob = rescale_none) +
+     # geom_text(aes(label = max_kills, x=year, y = max_kills), position = position_dodge(width = 0.75), vjust = -0.6, size = 4) + 
+     scale_fill_manual(values=colorPalette) +
+     theme_plot() + 
+     ggtitle(label = "Missouri hitting percentage") 
+     # ylab("Single-match high")
+print(p_missouri_hit_pcntg)
+
