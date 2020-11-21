da6b66bc033451c10faa0a71d68f7dc22b4ed2a3
Carlos <carlosgg123@gmail.com>
2020-11-21 09:07:52 -0500

    Update 2020-11-20 Part 2

--- Women/wordclouds.r
+++ Women/wordclouds.r
@@ -7,9 +7,12 @@ df2 <- as.data.frame(table(all_players$High_School[all_players$High_School_in_PR
 df3 <- as.data.frame(table(all_players$Institution))
 
 # High schools wordcloud
-wordcloud2(df2, size = 0.45,color = "random-dark", rotateRatio = 0, minSize = 0.2)
+colorVec = rep(brewer.pal(n=5, name = "Set1"), length.out=nrow(df2))
+wordcloud2(df2, size = 0.45,color = colorVec, rotateRatio = 0, minSize = 0.2)
 # Institutions wordcloud
 # After df3 wordcloud go to GIMP and rotate it
-wordcloud2(df3, size = 0.28,color = "random-dark", rotateRatio = 1, minRotation = pi/2, maxRotation = pi/2, minSize = 0.2)
+colorVec = rep(brewer.pal(n=5, name = "Set1"), length.out=nrow(df3))
+wordcloud2(df3, size = 0.28,color = colorVec, rotateRatio = 1, minRotation = pi/2, maxRotation = pi/2, minSize = 0.2)
 # Hometowns wordcloud
-wordcloud2(df, size = 1.3,color = "random-dark", rotateRatio = 0, minSize = 0.2)
+colorVec = rep(brewer.pal(n=5, name = "Set1"), length.out=nrow(df))
+wordcloud2(df, size = 1,color = colorVec, rotateRatio = 0, minSize = 0.2)
