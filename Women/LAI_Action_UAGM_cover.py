1f53f59d49bbc8ad36655d2c07de5724c693d212
Carlos <carlosgg123@gmail.com>
2020-11-23 12:29:33 -0500

    Update 2020-11-23

---
+++ Women/LAI_Action_UAGM_cover.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scribus import *
+margins = (0, 0, 0, 0)
+
+def text_frame(pos_x, pos_y, frame_width, frame_height, texts_list, fonts_list, font_sizes, colors, alignment = ALIGN_CENTERED, line_spacing = 24):
+  this_text_frame = createText(pos_x, pos_y, frame_width, frame_height)
+  insertText(texts_list[0] + "\n", -1, this_text_frame)
+  setFont(fonts_list[0], this_text_frame); setFontSize(font_sizes[0], this_text_frame); setTextColor(colors[0], this_text_frame)
+  previous_line_length = getTextLength(this_text_frame)
+  for idx in range(1,len(texts_list)):
+    text_line = texts_list[idx]
+    length_text_line = len(text_line)
+    if idx < len(texts_list) - 1: insertText(text_line + "\n", -1, this_text_frame)
+    else: insertText(text_line, -1, this_text_frame)
+    selectText(previous_line_length, length_text_line, this_text_frame)
+    setFont(fonts_list[idx], this_text_frame)
+    selectText(previous_line_length, length_text_line, this_text_frame)
+    setFontSize(font_sizes[idx], this_text_frame)
+    selectText(previous_line_length, length_text_line, this_text_frame)
+    setTextColor(colors[idx], this_text_frame)
+    selectText(previous_line_length, length_text_line, this_text_frame)
+    setTextAlignment(alignment, this_text_frame)
+    previous_line_length += length_text_line + 1
+  setTextAlignment(alignment, this_text_frame); setLineSpacing(line_spacing, this_text_frame)
+
+def draw_line(x1, y1, x2, y2, line_type = LINE_DASH, width = 1, color = "Map Blue"):
+  this_line = createLine(x1, y1, x2, y2); setLineStyle(line_type, this_line); setLineColor(color, this_line)
+  setLineWidth(width, this_line)
+  
+def insert_image(pos_x, pos_y, img_width, img_height, location):
+  this_image = createImage(pos_x, pos_y, img_width, img_height); loadImage(location, this_image); setScaleImageToFrame(1, 1, this_image)
+
+if newDocument(PAPER_LETTER, margins, PORTRAIT, 1,  UNIT_POINTS, NOFACINGPAGES, FIRSTPAGERIGHT, 1):
+
+  defineColor("UAGM", 5, 239, 207, 13)
+  top_rect = createRect(0, 0, 612, 36)
+  setFillColor("UAGM", top_rect); setLineColor("UAGM", top_rect)
+  bottom_rect = createRect(0, 756, 612, 36)
+  setFillColor("UAGM", bottom_rect); setLineColor("UAGM", bottom_rect)
+  text_frame(0, 5, 612, 36, ["Universidad Ana G. Méndez"], ["OLD SPORT 02 ATHLETIC NCV Regular"], [24], colors = ["White"])
+  text_frame(0, 6.7, 36, 36, ["2019", "-", "2020"], ["OLD SPORT 02 ATHLETIC NCV Regular" for idx in range(3)], [11,11, 11], 
+             colors = ["White" for idx in range(3)], line_spacing = 7)
+  text_frame(576, 6.7, 36, 36, ["2019", "-", "2020"], ["OLD SPORT 02 ATHLETIC NCV Regular" for idx in range(3)], [11,11, 11], 
+             colors = ["White" for idx in range(3)], line_spacing = 7)
+  insert_image(0, 36, 613, 721, "UAGM_cover.jpg")
+  text_frame(0, 760, 612, 40, ["Alejandra Argüello"], ["Playball Regular"], [32], colors = ["White"])
